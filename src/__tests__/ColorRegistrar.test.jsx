4849e4e2ecc71b21c67d542efa09d491e1cd5950
frosskiel <frosskiel@gmail.com>
2025-05-29 08:37:52 -0300

    add: complete ColorRegistrar.test

--- src/__tests__/ColorRegistrar.test.jsx
+++ src/__tests__/ColorRegistrar.test.jsx
@@ -3,7 +3,17 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ColorRegistrar from '../ColorRegistrar.jsx';
 
-test('titulo renderizado', () => {
+test('titulo', () => {
     render(<ColorRegistrar/>);
     expect(screen.getByText('REGISTRAR COLOR')).toBeInTheDocument();
+})
+
+test('nombre', () => {
+    render(<ColorRegistrar/>);
+    expect(screen.getByText('NOMBRE')).toBeInTheDocument();
+})
+
+test('descripcion', () => {
+    render(<ColorRegistrar/>);
+    expect(screen.getByText('DESCRIPCIÓN')).toBeInTheDocument();
 })
