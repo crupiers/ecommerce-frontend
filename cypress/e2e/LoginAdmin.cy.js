40b8aa31c44d204b386cf645150ca900e05028ba
Nacho <ignacio.rover@gmail.com>
2025-05-29 15:58:30 -0300

    update:loginAdmin.cy & add:logout.cy, register.cy

--- cypress/e2e/LoginAdmin.cy.js
+++ cypress/e2e/LoginAdmin.cy.js
@@ -1,8 +1,26 @@
 describe('LoginAdmin', () => {
   it('logueo correcto', () => {
-    cy.visit('http://localhost:5173/auth/login');
-    cy.get('input[name="nombre"]').type('admin');
-    cy.get('input[name="contrasenia"]').type('Hola1234_');
+    //register a new user
+    cy.visit('http://localhost:5173/auth/register');
+    const validUsername = `user-${Date.now()}`;
+    cy.get('input[name="nombre"]').type(validUsername);
+    const validPassword = 'Hola1234_';
+    cy.get('input[name="contrasenia"]').type(validPassword);
     cy.get('button[type="submit"').click();
+
+    //logout
+    cy.get('.bi-person-fill').click();
+    cy.get('button.me-2.align-items-center.d-flex').click()
+
+    //corroborar que va a la pagina de login
+    cy.url().should('include', '/auth/login');
+
+    //login
+    cy.get('input[name="nombre"]').type(validUsername);
+    cy.get('input[name="contrasenia"]').type(validPassword);
+    cy.get('button[type="submit"').click();
+
+    //corroborar que va al catalogo
+    cy.url().should('include', '/catalogo');
   })
 })
