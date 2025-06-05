7ac84263916745504a1de16430745e9f3a93811f
frosskiel <frosskiel@gmail.com>
2025-06-05 09:52:30 -0300

    chg: AdminUser.cy

--- cypress/e2e/AdminUser.cy.js
+++ cypress/e2e/AdminUser.cy.js
@@ -1,26 +1,53 @@
-describe('AdminRegisterLogin', () => {
-  it('registro y logueo correctos', () => {
+//PARA ESTE TEST NO DEBE EXISTIR EL USUARIO ADMIN EN LA BASE DE DATOS
+describe('AdminUser', () => {
+  it('registro, logueo y deslogueo correctos', () => {
     //register a new user
-    cy.visit('http://localhost:5173/auth/register');
+    cy.wait(1000);
+    cy.visit('http://localhost:5173/');
+    cy.wait(1000);
+    cy.get('a[href*="/auth/register"]').click();
+
+    cy.wait(1000);
+    
     const validUsername = `admin`;
     cy.get('input[name="nombre"]').type(validUsername);
+    cy.wait(1000);
+
     const validPassword = 'Hola1234_';
     cy.get('input[name="contrasenia"]').type(validPassword);
+    cy.wait(1000);
+
     cy.get('button[type="submit"').click();
+
+    cy.wait(1000);
 
     //logout
     cy.get('.bi-person-fill').click();
+    cy.wait(1000);
     cy.get('button.me-2.align-items-center.d-flex').click()
 
     //corroborar que va a la pagina de login
     cy.url().should('include', '/auth/login');
 
+    cy.wait(1000);
+    
     //login
     cy.get('input[name="nombre"]').type(validUsername);
+    cy.wait(1000);
     cy.get('input[name="contrasenia"]').type(validPassword);
+    cy.wait(1000);
     cy.get('button[type="submit"').click();
 
     //corroborar que va al catalogo
     cy.url().should('include', '/catalogo');
+
+    cy.wait(1000);
+
+    // Click on the username dropdown
+    cy.get('.bi-person-fill').click();
+    cy.wait(1000);
+
+    // Click on the logout button using className
+    cy.get('button.me-2.align-items-center.d-flex').click();
   })
 })
