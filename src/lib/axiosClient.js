8eecb71755e192f1d2ec34519c06f02afbe957aa
frosskiel <frosskiel@gmail.com>
2025-06-11 08:43:18 -0300

    fix: ip back

--- src/lib/axiosClient.js
+++ src/lib/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const AXIOS_CLIENT = axios.create({
-    baseURL: "http://localhost:8080/ecommerce",
+    baseURL: "http://192.168.56.101:8080/ecommerce",
     headers: {"Access-Control-Allow-Origin": "http://localhost:5173"}
 });
 
